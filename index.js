dd9611a982b3b00dd8dded3b59f35008f3caffd4
Roman Kachura <roma.kachyra@gmail.com>
2024-01-03 19:30:36 +0300

    added cors

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const jsonServer = require('json-server');
 const cors = require('cors');
 const path = require('path');
-const server = jsonServer.create();
+const index = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults({
   static: path.join(__dirname, 'build')
 });
 const PORT = process.env.PORT || 3030;
 
-server.use(cors())
-server.use(jsonServer.bodyParser)
-server.use(middlewares)
-server.use(router)
+index.use(cors())
+index.use(jsonServer.bodyParser)
+index.use(middlewares)
+index.use(router)
 
-server.use(router);
-server.listen(PORT, () => {
+index.use(router);
+index.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
